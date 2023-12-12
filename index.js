3a8c16dfa5a6431e4a2bd1e2b5ca4d73fba87bbd
Samuel Omoding <samuel.omoding@student.moringaschool.com>
2023-12-12 12:26:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`);
+}
+
+function mondayWork(activity = 'go to the office'){
+    return(`This Monday, I will ${activity}.`);
+};
+
+function wrapAdjective(sign){
+    return function(adjective){
+        return (`You are ${sign}${adjective}${sign}!`);
+    };
+    }
+
+const encouragingPromptFunction = wrapAdjective("!!!");
+console.log(encouragingPromptFunction("a dedicated programmer"));
